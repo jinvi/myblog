2308595600fa5758b32b78632251e1801940f44e
jun <jun@linseek.com>
2018-07-18 17:04:33 +0800

    1

--- config/production.js
+++ config/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongodb: 'mongodb://jinvi:d111111@ds141621.mlab.com:41621/myblogdatabase'
+    mongodb: 'mongodb://jinvi:d111111@ds141621.mlab.com:41621/myblogdatabase'  // [用户名：密码] 登录mlab.com数据库
 }
